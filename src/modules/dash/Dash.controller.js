5b2720a502268ea75d0f24c2eafef4079b74725c
Balint Vekerdy <balint@dangerfarms.com>
2015-07-02 22:30:12 +0200

    initial commit

--- src/modules/dash/Dash.controller.js
+++ src/modules/dash/Dash.controller.js
@@ -2,12 +2,50 @@
 
   class DashController {
 
-    constructor($scope) {
+    constructor($scope, $timeout, $cordovaBarcodeScanner) {
       this.$scope = $scope;
+      this.$timeout = $timeout;
+      this.$cordovaBarcodeScanner = $cordovaBarcodeScanner;
+      ionic.Platform.ready(this.startScanning.bind(this));
     }
+
+    startScanning() {
+      this.$cordovaBarcodeScanner
+        .scan()
+        .then(
+        this.scanSuccess.bind(this),
+        this.scanError.bind(this)
+      );
+    }
+
+    scanSuccess(barcodeData) {
+      console.log(barcodeData);
+      this.barcode = barcodeData;
+      this.$timeout(
+        this.startScanning.bind(this),
+        2000
+      )
+    }
+
+    scanError(error) {
+      console.error(error);
+    }
+
   }
 
+
+  DashController.$inject = [
+    '$scope',
+    '$timeout',
+    '$cordovaBarcodeScanner',
+  ];
+
+  let dependencies = [
+    'ionic',
+    'ngCordova'
+  ];
+
   angular
-    .module('starter.dash.controller', [])
+    .module('starter.dash.controller', dependencies)
     .controller('DashController', DashController);
 })();
